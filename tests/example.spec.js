acb741d17e35111bf65af5ddb6bd7a9b15054be5
manianbu5700 <manianbu5700@gmail.com>
2026-07-09 15:59:28 +0530

    modified

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,6 +3,6 @@ import { test } from '@playwright/test';
 
 test('validate facebook application', async ({ page }) => {
   await page.goto('https://www.facebook.com/');
-  console.log("Hii Buddy")
+  console.log("Hii")
 });
 
